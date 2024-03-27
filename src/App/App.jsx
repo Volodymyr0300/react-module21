ec46e4b2cf15681cda2fa30ae4fa92eb9f4004c2
Volodymyr Poliakov <vladimir03002@gmail.com>
2024-03-27 11:43:01 -0500

    raising state

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,30 +1,19 @@
-import css from "../App/App.module.css";
 import { useState } from "react";
 import ClickCounter from "./ClickCounter/ClickCounter";
 
-function App() {
+const App = () => {
   const [clicks, setClicks] = useState(0);
-  const [isOpen, setIsOpen] = useState(false);
 
   const handleClick = () => {
     setClicks(clicks + 1);
   };
 
-  const handleToggle = () => {
-    setIsOpen(!isOpen);
-  };
-  console.log(isOpen);
-  console.log(!isOpen);
-
   return (
     <>
-      <button onClick={handleClick}>Current: {clicks}</button>
-      <button onClick={handleToggle}>{isOpen ? "Hide" : "Show"}</button>
-      {isOpen && <p>Now you can see me!</p>}
-      <ClickCounter />
-      <ClickCounter />
+      <ClickCounter value={clicks} onUpdate={handleClick} />
+      <ClickCounter value={clicks} onUpdate={handleClick} />
     </>
   );
-}
+};
 
 export default App;
