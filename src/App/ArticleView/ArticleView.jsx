1bd7cca5d6c89dadff8fe8ba3adc33f062f21529
Volodymyr Poliakov <vladimir03002@gmail.com>
2024-03-27 21:52:11 -0500

    article2

---
+++ src/App/ArticleView/ArticleView.jsx
@@ -0,0 +1,10 @@
+export default function ArticleView({ article }) {
+  return (
+    <>
+      <article>
+        <h1>{article.topic}</h1>
+        <p>{article.text}</p>
+      </article>
+    </>
+  );
+}
